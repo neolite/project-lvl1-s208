7b05e7b8ecaf008516084c798dd42d27ab4c4869
Rafkat Galiullin <ufadiz@gmail.com>
2017-11-27 00:31:17 +0500

    [feat] add progression

---
+++ src/games/progression.js
@@ -0,0 +1,33 @@
+import { getRandom, gameFactory } from '..';
+
+const getProgression = () => {
+  const step = getRandom(1, 10);
+  let progressionString = '';
+  let count = 0;
+  const indexOfHidden = getRandom(0, 9);
+  let hiddenNumber = 0;
+  for (let i = 0; i < 10; i += 1) {
+    count += step;
+    if (indexOfHidden === i) {
+      hiddenNumber = String(count);
+      progressionString += '.. ';
+    } else {
+      progressionString += `${count} `;
+    }
+  }
+  return { progressionString, hiddenNumber };
+};
+
+const progressionGame = () => {
+  const { progressionString, hiddenNumber } = getProgression();
+  return {
+    question: progressionString,
+    answer: hiddenNumber,
+  };
+};
+
+const gameWrapper = () => {
+  gameFactory(progressionGame, 'What number is missing in this progression?');
+};
+
+export default gameWrapper;
