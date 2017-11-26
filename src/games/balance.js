a36478526a2934f8714ccdda1ac5358fba046e35
Rafkat Galiullin <ufadiz@gmail.com>
2017-11-26 18:02:53 +0500

    [feat] add balance game

---
+++ src/games/balance.js
@@ -0,0 +1,29 @@
+import { getRandom, gameFactory } from '..';
+
+const getBalanced = (n) => {
+  let digits = Array.isArray(n) ? n : Array.from(n.toString()).map(Number);
+  const nMax = Math.max(...digits);
+  const nMin = Math.min(...digits);
+  if ((nMax - nMin) > 1) {
+    digits[digits.indexOf(nMin)] = nMin + 1;
+    digits[digits.indexOf(nMax)] = nMax - 1;
+    digits = digits.sort((a, b) => a - b);
+    return getBalanced(digits);
+  }
+  return n.reduce((el, acc) => el.toString() + acc);
+};
+
+const balanceGame = () => {
+  const randomNumber = getRandom(1, 1000);
+  const answer = getBalanced(randomNumber);
+  return {
+    question: `${randomNumber}`,
+    answer,
+  };
+};
+
+const gameWrapper = () => {
+  gameFactory(balanceGame, 'Balance the given number.');
+};
+
+export default gameWrapper;
